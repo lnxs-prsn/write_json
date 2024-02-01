d4685a153f80edac4ae20551e67c00921615d6fe
Live session user <ahmsa914@gmail.com>
2024-02-01 16:05:40 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::vec;
+
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+
+struct Paragraph {
+    name: String
+
+}
+
+#[derive(Serialize, Deserialize)]
+
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+
+}
+
+fn main(){
+    let article: Article = Article{
+        article: String::from("how to wotk with json in rust"),
+        author: String::from("akhil"),
+        paragraph: vec![
+            Paragraph{
+                name: String::from("first sentence")
+            },
+            Paragraph{
+                name: String::from("body of the paragraph")
+            },
+            Paragraph{
+                name: String::from("end of the paragraph")
+            }
+        ]
+    };
+    let json = serde_json::to_string(&article).unwrap();
+    println!("the json is: {}", json)
+}
